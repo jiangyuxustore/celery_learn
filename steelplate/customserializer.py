cc0fd08de67ec1b74be7614eeac3a669c22f4edb
jiangyuxu <jiangyuxu0039@gmail.com>
2023-03-08 17:06:50 +0800

    feat: 解决Apps aren't loaded yet.报错

---
+++ steelplate/customserializer.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from steelplate import models
+
+
+class SteelOriginalInfoSerializer(serializers.Serializer):
+    task_id = serializers.CharField(max_length=128)
+    line_id = serializers.IntegerField()
+    board_thick = serializers.CharField(max_length=128)
+    board_type = serializers.CharField(max_length=128)
+    task_status = serializers.CharField(max_length=50)
+    total_count = serializers.IntegerField(default=0)
+    active_count = serializers.IntegerField(default=0)
+    success_count = serializers.IntegerField(default=0)
+    failed_count = serializers.IntegerField(default=0)
+    json_path = serializers.CharField(max_length=256)
+    dxf_path = serializers.CharField(max_length=256)
+    plate_img = serializers.CharField(max_length=500)
+    plate_size = serializers.CharField(max_length=256)
+    start_time = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
+    is_manual_success = serializers.IntegerField(default=0)
+    is_manual_generate = serializers.IntegerField(default=0)
+    is_delete = serializers.IntegerField(default=0)
+
+    def create(self, validated_data):
+        return models.SteelOriginalInfo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+
+    class Meta:
+        model = models.SteelOriginalInfo
+        fields = "__all__"
+        ordering = ["created_time"]
---
+++ steelplate/tasks.py
@@ -0,0 +1,24 @@
+"""celery异步任务必须定义在每个app的tasks.py中, 然后celery会自动加载每个app中的tasks.py"""
+import django
+django.setup()
+from steelplate.customserializer import SteelOriginalInfoSerializer
+from celery import Task
+from steelplate.models import SteelOriginalInfo
+
+
+class SteelOriginalInfoOperator(Task):
+
+    name = "SteelOriginalInfoOperator"
+
+    def run(self, method, data, *args, **kwargs):
+        if method == "POST":
+            print("开始执行序列化")
+            serializer_instance = SteelOriginalInfoSerializer(data=data)
+            serializer_instance.is_valid()
+            serializer_instance.save()
+        elif method == "PUT":
+            pass
+
+        elif method == "DELETE":
+            pass
+
