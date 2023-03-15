a59198325cdefd4127d93bc2eca0a31c2cdb10e5
jiangyuxu <jiangyuxu0039@gmail.com>
2023-03-15 18:05:50 +0800

    feat: 测试dead letter exchange

--- blog/tasks.py
+++ blog/tasks.py
@@ -153,7 +153,7 @@ def on_failed(self, retval, task_id, args, kwargs):
     print('任务执行失败了')
 
 
-@shared_task(name='blog.function_base_add', bind=True, queue='topic_queue', on_failed=on_failed)
+@shared_task(name='blog.function_base_add', bind=True, acks_late=True, on_failed=on_failed)
 def function_base_add(self, x, y):
     """
     bind=True, 则第一个参数就是class base task中的self实例, 然后你在下面就可以用self.retry了
