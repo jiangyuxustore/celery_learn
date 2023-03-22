005402d4484e7f7f0ab187c1057783969f2e251d
jiangyuxu <jiangyuxu0039@gmail.com>
2023-03-22 17:28:54 +0800

    feat: 新增打包镜像的文件

--- startflask.sh
+++ startflask.sh
@@ -1,4 +1,5 @@
 #！/bin/bash
 echo start python
-nohup python3 /home/dxflearn/flask_server.py > /dev/null 2>&1 &
+# nohup python3 /home/dxflearn/flask_server.py > /dev/null 2>&1 &
+python3 /home/dxflearn/flask_server.py
 echo end python
